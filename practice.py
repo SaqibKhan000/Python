ae32b2965d719ad65e46412c44e70cf6e17d4f47
M Saqib Khan <saqibkhannn543@gmail.com>
2026-05-16 17:05:26 +0500

    Dummy code

--- practice.py
+++ practice.py
@@ -1,6 +1,6 @@
-password = ""
 
-while len(password) < 8:
-    password = input("Enter password (8+ characters): ")
-
-print("Password accepted")
+x = 10
+def func():
+    x = 99
+    print(x)
+print(x)
