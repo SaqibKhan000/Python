d0fc5e10150de400d87d907e1a887bcf0d875799
M Saqib Khan <saqibkhannn543@gmail.com>
2026-05-29 19:58:47 +0500

    Inheritance and Polymorphism

--- practice.py
+++ practice.py
@@ -1,37 +1,15 @@
-class Student:
-    school = "SMIT Peshawar Student Report"
-    def __init__(self, name, roll_no, marks):
+class Animal:
+    def __init__(self, name, age):
         self.name = name
-        self.roll_no = roll_no
-        self.marks = marks
+        self.age = age
 
-    def average(self):
-        return sum(self.marks) / len(self.marks)
-    
-    def grade(self):
-        avg = self.average()
-        if avg >= 90:
-            return "A"
-        elif avg >= 80:
-            return "B"
-        elif avg >= 70:
-            return "C"
-        elif avg >= 60:
-            return "D"
-        else:
-            return "F"
-    
-    def report(self):
-        return (f"-------- {self.school} Report ----\n"
-                f"Name: {self.name}\n"
-                f"Roll No: {self.roll_no}\n"
-                f"Average: {self.average():.2f}\n"
-                f"Grade: {self.grade()}")
-    
-    def __str__(self):
-        return f"Student:{self.name}, Roll No: {self.roll_no}"
-    
-std1 = Student("Ali", 102, [85, 90, 78, 95])
-std2 = Student("Sara", 105, [92, 88, 91, 89])
-print(std1.report())
-print(std2.report())
+    def eat(self):
+        print(self.name, "is eating")
+
+class Dog(Animal):
+    def __init__(self, name, age, breed):
+        super().__init__(name, age)
+        self.breed = breed
+
+d = Dog("Buddy", 5, "Golden")
+d.eat()
