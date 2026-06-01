ae64b593db6eb8548d39c0aef68274a5ad34cb61
M Saqib Khan <saqibkhannn543@gmail.com>
2026-06-01 18:00:02 +0500

    JSON Methods

--- practice.py
+++ practice.py
@@ -1,9 +1,7 @@
-num1 = 10
-num2 = 10
-try:
-    print(num1 / num2)
-except Exception:
-    print("Error occurred!")
-else:
-    print("This is else block!")
-finally:    print("This is finally block!")
+import json
+
+
+
+with open("data.json", "r") as file:
+   data = json.load(file)
+print(data)
