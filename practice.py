64c1bfa176144c3950e90fd63321b6209d93c87a
M Saqib Khan <saqibkhannn543@gmail.com>
2026-06-04 13:59:45 +0500

    List in Python

--- practice.py
+++ practice.py
@@ -1,3 +1,4 @@
-
-name = "MSaqibKhan"
-print(name[::-1])
+list26 = [[1, 2], [3, 4], [5, 6]]
+def flatten_list(list):
+    return [item for item in list for item in item]
+print(flatten_list(list26))
